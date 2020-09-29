956105dcfb81383d251b3e3e70e5e602988ba3fe
shivamj2603 <shivamj2603@gmail.com>
2020-09-30 00:38:58 +0530

    Added:Functionality to Validate Last Name

--- UserRegistration.java
+++ UserRegistration.java
@@ -5,25 +5,29 @@ import java.util.regex.*;
 public class UserRegistration
 {
 	
-        public static void validateFirstName(String firstName)
+        public static void validateName(String name)
 	{
 		String regex = "^[A-Z]{1}[a-zA-z]{2,}";
 		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(firstName);
+		Matcher matcher = pattern.matcher(name);
 
 		if(matcher.find()) {
-			System.out.println(firstName + " is valid");
+			System.out.println(name + " is valid");
 		}
                 else {
-			System.out.println(firstName + " is not valid");
+			System.out.println(name + " is not valid");
                 }
 	}
 	public static void main(String[] args) {
 		String firstName = "";
+		String lastName = "";
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter the first name");
 		firstName = input.nextLine();
-		validateFirstName(firstName);	
+		validateName(firstName);//validation of first name	
+		System.out.println("Enter the last name");
+		lastName = input.nextLine();
+		validateName(lastName);//validation of last name
 	}
 
 }
