1e1829ed92015bfd5b4c31c2b4ac6f7bf71ea0a4
shivamj2603 <shivamj2603@gmail.com>
2020-09-30 07:55:30 +0530

    Added:Functionality to validate Password

--- UserRegistration.java
+++ UserRegistration.java
@@ -40,11 +40,26 @@ public class UserRegistration
 		else
 			System.out.println(mobileNumber + " is not valid");
 	}
+	public static void validatePassword(String password)
+    {
+        String regex="[a-zA-Z0-9@_-#$%^&*]{8,}";
+        Pattern pattern=Pattern.compile(regex);
+        Matcher matcher=pattern.matcher(password);
+
+        if(matcher.find()) {
+            System.out.println(password+" is valid");
+        }
+        else {
+            System.out.println(password+" is not valid");
+        }
+    }
+
 	public static void main(String[] args) {
 		String firstName = "";
 		String lastName = "";
 		String email = "";
 		String mobileNumber = "";
+		String password = "";
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter the first name");
 		firstName = input.nextLine();
@@ -58,6 +73,9 @@ public class UserRegistration
 		System.out.println("Enter the Mobile Number");
 		mobileNumber = input.nextLine();
 		validateMobile(mobileNumber);//validation of MobileNumber
+		System.out.println("Enter the password");
+		password = input.nextLine();
+		validatePassword(password);
 	}
 
 }
