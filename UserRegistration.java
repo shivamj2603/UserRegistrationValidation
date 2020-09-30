81b16cff66a7ce96c455a23eba74b5e3a59c13e2
shivamj2603 <shivamj2603@gmail.com>
2020-09-30 07:43:25 +0530

    Added:Functionality to validate Email

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,8 +4,8 @@ import java.util.regex.*;
 
 public class UserRegistration
 {
-	
-        public static void validateName(String name)
+
+	public static void validateName(String name)
 	{
 		String regex = "^[A-Z]{1}[a-zA-z]{2,}";
 		Pattern pattern = Pattern.compile(regex);
@@ -14,13 +14,25 @@ public class UserRegistration
 		if(matcher.find()) {
 			System.out.println(name + " is valid");
 		}
-                else {
+		else {
 			System.out.println(name + " is not valid");
-                }
+		}
+	}
+	public static void validateEmail(String Email) {
+		String expression = "^abc?[.][A-Za-z]*@bl[.]co[.][A-Za-z]{2,}$"; // Pattern for Email
+		Pattern pattern = Pattern.compile(expression);
+		Matcher match = pattern.matcher(Email);
+		if(match.find()) {
+			System.out.println("Email is Valid");
+		}
+		else {
+		System.out.println("Email is invalid");
+		}
 	}
 	public static void main(String[] args) {
 		String firstName = "";
 		String lastName = "";
+		String email = "";
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter the first name");
 		firstName = input.nextLine();
@@ -28,6 +40,9 @@ public class UserRegistration
 		System.out.println("Enter the last name");
 		lastName = input.nextLine();
 		validateName(lastName);//validation of last name
+		System.out.println("Enter the Email");
+		email = input.nextLine();
+		validateEmail(email);//validation of Email
 	}
 
 }
