992c16a985f282feabfb9dcf755d5b682e81508a
shivamj2603 <shivamj2603@gmail.com>
2020-10-01 08:08:28 +0530

    Modified:Ability to verify emails

--- UserRegistration.java
+++ UserRegistration.java
@@ -5,7 +5,7 @@ import java.util.regex.*;
 public class UserRegistration
 {
 
-	public static void validateName(String name)
+	public static String validateName(String name)
 	{
 		String regex = "^[A-Z]{1}[a-zA-z]{2,}";
 		Pattern pattern = Pattern.compile(regex);
@@ -17,19 +17,21 @@ public class UserRegistration
 		else {
 			System.out.println(name + " is not valid");
 		}
+		return null;
 	}
-	public static void validateEmail(String Email) {
+	public static String validateEmail(String Email) {
 		String expression = "^abc?[.][A-Za-z]*@bl[.]co[.][A-Za-z]{2,}$"; // Pattern for Email
 		Pattern pattern = Pattern.compile(expression);
 		Matcher match = pattern.matcher(Email);
 		if(match.find()) {
-			System.out.println("Email is Valid");
+			System.out.println("Valid");
 		}
 		else {
-			System.out.println("Email is invalid");
+			System.out.println("invalid");
 		}
+		return null;
 	}
-	public static void validateMobile(String mobileNumber)
+	public static String validateMobile(String mobileNumber)
 	{
 		String regex="[0-9]{2}[ ][1-9][0-9]{9}";
 		Pattern pattern=Pattern.compile(regex);
@@ -39,22 +41,49 @@ public class UserRegistration
 			System.out.println(mobileNumber + " is valid");
 		else
 			System.out.println(mobileNumber + " is not valid");
+		return null;
 	}
-	public static void validatePassword(String password)
-    {
-        String regex="^(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*?&])[a-zA-Z0-9@_-#$%^&*]{8,}$";
-        Pattern pattern=Pattern.compile(regex);
-        Matcher matcher=pattern.matcher(password);
+	public static String validatePassword(String password)
+	{
+		String regex = "(?=.*[A-Z])(?=.*[0-9])(?=.[@#$%^&*])[A-Z-a-z0-9]{8,}$";
+		Pattern pattern=Pattern.compile(regex);
+		Matcher matcher=pattern.matcher(password);
 
-        if(matcher.find()) {
-            System.out.println(password + " is valid");
-        }
-        else {
-            System.out.println(password + " is not valid");
-        }
-    }
-
+		if(matcher.find()) {
+			System.out.println(password + " is valid");
+		}
+		else {
+			System.out.println(password + " is not valid");
+		}
+		return null;
+	}
+	private void verifyEmail() {
+		//Valid email
+		validateEmail("abc-100@yahoo.com");
+		validateEmail("abc.100@yahoo.com");
+		validateEmail("abc111@abc.com");
+		validateEmail("abc-100@abc.net");
+		validateEmail("abc.100@abc.com.au");
+		validateEmail("abc@1.com");
+		validateEmail("abc@gmail.com.com");
+		validateEmail("abc+100@gmail.com");
+		//Invallid email
+		validateEmail("abc");
+		validateEmail("abc@.com.my");
+		validateEmail("abc123@gmail.a");
+		validateEmail("abc123@.com");
+		validateEmail("abc123@.com.com");
+		validateEmail(".abc@abc.com");
+		validateEmail("abc()*@gmail.com");
+		validateEmail("abc@%*.com");
+		validateEmail("abc..2002@gmail.com");
+		validateEmail("abc.@gmail.com");
+		validateEmail("abc@abc@gmail.com");
+		validateEmail("abc@gmail.com.1a");
+		validateEmail("abc@gmail.com.aa.au");
+	}
 	public static void main(String[] args) {
+		UserRegistration user = new UserRegistration();
 		String firstName = "";
 		String lastName = "";
 		String email = "";
@@ -63,19 +92,20 @@ public class UserRegistration
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter the first name");
 		firstName = input.nextLine();
-		validateName(firstName);//validation of first name	
+		user.validateName(firstName);//validation of first name	
 		System.out.println("Enter the last name");
 		lastName = input.nextLine();
-		validateName(lastName);//validation of last name
+		user.validateName(lastName);//validation of last name
 		System.out.println("Enter the Email");
 		email = input.nextLine();
-		validateEmail(email);//validation of Email
+		user.validateEmail(email);//validation of Email
 		System.out.println("Enter the Mobile Number");
 		mobileNumber = input.nextLine();
-		validateMobile(mobileNumber);//validation of MobileNumber
+		user.validateMobile(mobileNumber);//validation of MobileNumber
 		System.out.println("Enter the password");
 		password = input.nextLine();
-		validatePassword(password);//validation of password
+		user.validatePassword(password);//validation of password
+		user.verifyEmail();
 	}
 
 }
